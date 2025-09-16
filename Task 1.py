32a44be1feb10ab6cfb137d2bec3ab25caebbc81
AbhishekPrabhu663 <166095243+AbhishekPrabhu663@users.noreply.github.com>
2025-09-16 20:25:35 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,12 @@
+F=int(input("Enter the First Number: "))
+S=int(input("Enter the Second Number: "))
+
+A=F+S
+s=F-S
+M=F*S
+D=F/S
+
+print("Addition:", A)
+print("Subtraction:", s)
+print("Multiplication:", M)
+print("Division:", D)
---
+++ Task 2.py
@@ -0,0 +1,4 @@
+f=input("Enter Your First Name: ")
+s=input("Enter Your Second Name: ")
+c=f+s
+print("Hello", c ,"This is Your Second Python Task" )
