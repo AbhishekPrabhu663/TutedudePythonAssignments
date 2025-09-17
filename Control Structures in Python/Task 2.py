7175241172e20e341e34dfee6c5fe07d727fef18
AbhishekPrabhu663 <166095243+AbhishekPrabhu663@users.noreply.github.com>
2025-09-17 21:26:28 +0530

    Update and rename Task 2.py to Control Structures in Python/Task 2.py

---
+++ Control Structures in Python/Task 2.py
@@ -0,0 +1,5 @@
+sum=0
+
+for i in range(51):
+    sum=sum+i
+print("The sum if numbers from 1 to 50 is:",sum)
--- Task 2.py
+++
@@ -1,5 +0,0 @@
-sum=0
-
-for i in range(51):
-    sum=sum+i
-print("The sum if numbers from 1 to 50 is:",sum)
